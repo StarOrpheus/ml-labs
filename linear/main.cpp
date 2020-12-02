cad24305b811658a613448a05e5cb3119aa3b201
Zakhar Koval <ellesterate@gmail.com>
2020-12-02 21:47:24 +0300

    Added simple solution

--- linear/main.cpp
+++ linear/main.cpp
@@ -10,8 +10,14 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <map>
 
-using precise_t = double;
+
+#ifdef ENABLE_TESTS
+    #include <filesystem>
+#endif
+
+using precise_t = float;
 
 using feature_vec = std::vector<precise_t>;
 
@@ -20,7 +26,7 @@ precise_t scalar(feature_vec const& lhs,
 {
     precise_t sum = 0; // std::transform_reduce is not available on CF
     assert(lhs.size() == rhs.size());
-//    #pragma omp simd
+
     for (size_t i = 0; i < lhs.size(); ++i)
         sum += lhs[i] * rhs[i];
     return sum;
@@ -29,9 +35,51 @@ precise_t scalar(feature_vec const& lhs,
 void muls(feature_vec& lhs,
           precise_t rhs)
 {
-//    #pragma omp simd
+    #pragma omp simd
     for (size_t i = 0; i < lhs.size(); ++i)
         lhs[i] *= rhs;
+}
+
+void subss(feature_vec& lhs,
+           feature_vec const& rhs)
+{
+    assert(lhs.size() == rhs.size());
+
+    #pragma omp simd
+    for (size_t i = 0; i < lhs.size(); ++i)
+        lhs[i] -= rhs[i];
+}
+
+void divss(feature_vec& lhs,
+           feature_vec const& rhs)
+{
+    assert(lhs.size() == rhs.size());
+
+    #pragma omp simd
+    for (size_t i = 0; i < lhs.size(); ++i)
+        lhs[i] /= rhs[i];
+}
+
+void minss(feature_vec& sum,
+           feature_vec const& Xs)
+{
+    assert(sum.size() == Xs.size());
+
+    #pragma omp simd
+    for (size_t i = 0; i < sum.size(); ++i)
+        if (Xs[i] < sum[i])
+            sum[i] = Xs[i];
+}
+
+void maxss(feature_vec& sum,
+           feature_vec const& Xs)
+{
+    assert(sum.size() == Xs.size());
+
+    #pragma omp simd
+    for (size_t i = 0; i < sum.size(); ++i)
+        if (Xs[i] > sum[i])
+            sum[i] = Xs[i];
 }
 
 std::ostream& operator<<(std::ostream& out, feature_vec const& v)
@@ -40,7 +88,7 @@ std::ostream& operator<<(std::ostream& out, feature_vec const& v)
     auto last = v.cend();
     for (auto it = first; it != last; ++it)
     {
-        out << std::fixed << std::setprecision(9) << *it;
+        out << std::fixed << std::numeric_limits<precise_t>::digits10 << *it;
         if ((it + 1) != last)
             out << " ";
     }
@@ -55,30 +103,36 @@ struct training_object
 
 struct single_run_result
 {
-    precise_t   s; // snape error result
-    feature_vec w;
+    precise_t   s; //!< smape error result
+    feature_vec w; //!< achieved weights
 };
 
 precise_t nz(precise_t x)
 {
-    if (x == 0)
-        return 1.0;
-    else
-        return x;
+    return (x == 0) ? 1.0 : x;
+}
+
+size_t sample_size(std::vector<training_object> const& objects)
+{
+    return std::min(objects.size(), (size_t) 100u);
 }
 
 /// The SMAPE error function
+template<typename PosGeneratorF>
 precise_t smape(std::vector<training_object> const& objects,
-                feature_vec const& W)
+               feature_vec const& W,
+               PosGeneratorF&& generator)
 {
     precise_t result = 0;
-    for (auto&& obj : objects)
+    size_t cnt = sample_size(objects);
+    for (size_t i = 0; i < cnt; ++i)
     {
+        auto& obj = objects[generator()];
         auto cur_target = scalar(obj.features, W);
         result += std::abs(cur_target - obj.target)
-                    / nz(std::abs(cur_target) + std::abs(obj.target));
+                  / nz(std::abs(cur_target) + std::abs(obj.target));
     }
-    result /= objects.size();
+    result /= cnt;
     return result;
 }
 
@@ -91,129 +145,281 @@ precise_t copy_sign(precise_t x_, precise_t y)
         return x;
 }
 
-precise_t dSMAPE2(feature_vec const& x,
-                  precise_t E,
-                  feature_vec const& W,
-                  size_t i)
+precise_t dLoss2(feature_vec const &x, 
+                 precise_t E,
+                 size_t i,
+                 precise_t F)
 {
     assert(i < x.size());
-    auto F = scalar(W, x);
-    auto num = x[i] * (copy_sign(1, F - E) * (std::abs(F) + std::abs(E)) - copy_sign(F - E, F));
+    auto num = x[i] * (copy_sign(std::abs(F) + std::abs(E), F - E) - copy_sign(F - E, F));
     auto a = std::abs(F) + std::abs(E);
     auto den = nz(a * a);
     return num / den;
 }
 
+struct normalizer
+{
+    explicit normalizer(std::vector<training_object> const& objs) noexcept
+    {
+        assert(objs.size());
+        x_mins = objs[0].features;
+        x_diff = objs[0].features;
+        y_min = objs[0].target;
+        y_diff = objs[0].target;
+        for (size_t i = 1; i < objs.size(); ++i)
+        {
+            minss(x_mins, objs[i].features);
+            maxss(x_diff, objs[i].features);
+            if (objs[i].target < y_min)
+                y_min = objs[i].target;
+            if (objs[i].target > y_diff)
+                y_diff = objs[i].target;
+        }
+
+        subss(x_diff, x_mins);
+        for (auto&& x : x_diff)
+            x = nz(x);
+        y_diff -= y_min;
+        y_diff = nz(y_diff);
+    }
+
+    void normalize(std::vector<training_object>& objs)
+    {
+        for (size_t i = 0; i < objs.size(); ++i)
+        {
+            auto& x = objs[i];
+            subss(x.features, x_mins);
+            divss(x.features, x_diff);
+
+            x.target -= y_min;
+            x.target /= y_diff;
+        }
+    }
+
+    feature_vec denormalize(feature_vec const& w_norm)
+    {
+        feature_vec w(w_norm.size());
+        
+        for (size_t i = 0; i < w_norm.size(); ++i)
+            w[i] = w_norm[i] * y_diff / x_diff[i];
+
+        precise_t sum = 0;
+        for (size_t i = 0; i < w_norm.size() - 1; ++i)
+            sum += w_norm[i] * y_diff * x_mins[i] / x_diff[i];
+        w.back() += y_min - sum;
+        return w;
+    }
+
+private:
+    feature_vec x_mins;
+    feature_vec x_diff;
+    precise_t   y_min;
+    precise_t   y_diff;
+};
+
 feature_vec solution(std::vector<training_object> objects,
                      size_t m)
 {
-    std::mt19937_64 rnd_dev{1337228};
-    std::uniform_real_distribution<> dis((precise_t) -1.0, (precise_t) 1.0);
+    normalizer norm(objects);
+    norm.normalize(objects);
 
-    single_run_result result{1000, feature_vec{0}};
+    std::mt19937_64 rnd_dev{1437687};
+    std::uniform_real_distribution<> W_init_dis(-0.5 / (precise_t) m, 0.5 / (precise_t) m);
+    std::uniform_int_distribution<size_t> sample_dis((size_t) 0, objects.size() - 1);
 
-    size_t run_cnt = 5;
+    single_run_result result{1000, feature_vec(m)};
+
+    size_t run_cnt = 10000;
     auto start = std::chrono::system_clock::now();
+    feature_vec W(m, 0);
+    std::vector<precise_t> grad(m);
+
     for (size_t run_id = 0; run_id < run_cnt; ++run_id)
     {
-        std::shuffle(objects.begin(), objects.end(), rnd_dev);
+//        std::shuffle(objects.begin(), objects.end(), rnd_dev);
+        std::generate(W.begin(), W.end(), std::bind(W_init_dis, rnd_dev));
 
-        feature_vec W(m);
-        std::generate(W.begin(), W.end(), std::bind(dis, rnd_dev));
-        muls(W, 1 / (2 * (precise_t) m));
-
-        for (size_t iter = 0; iter < 100; ++iter)
+        for (size_t iter = 0; iter < sample_size(objects); ++iter)
         {
-            training_object& obj = objects[iter % objects.size()];
+            training_object& obj = objects[sample_dis(rnd_dev)];
+            bool brk = true;
+            auto learning_rate = 1 / (precise_t) (iter + 1);
+            auto Forecast = scalar(W, obj.features);
+            precise_t regularization_tau = 0.008;
+
             for (size_t i = 0; i < m; ++i)
             {
-                auto mu = 1 / (precise_t) (iter + 1);
-//                precise_t d1 = dSMAPE1(obj.features, obj.target, W, i);
-                precise_t d1 = dSMAPE2(obj.features, obj.target, W, i);
-                W[i] -= mu * d1;
+                precise_t d1 = dLoss2(obj.features, obj.target, i, Forecast);
+                grad[i] = learning_rate * d1;
+                if (grad[i] != 0)
+                    brk = false;
             }
+            muls(W, 1 - learning_rate * regularization_tau);
+            subss(W, grad);
+            if (brk)
+                break;
         }
 
         assert(W.size() == m);
-        auto current_S = smape(objects, W);
-
+        auto current_S = smape(objects, W, std::bind(sample_dis, rnd_dev));
         if (current_S < result.s)
         {
             result.s = current_S;
-            result.w = std::move(W);
+            std::swap(result.w, W);
         }
 
-        if (std::chrono::system_clock::now() - start >= std::chrono::milliseconds(300))
+        if (std::chrono::system_clock::now() - start >= std::chrono::milliseconds(850))
             break;
     }
 
-    return result.w;
+    return norm.denormalize(result.w);
 }
 
-void read_objects(std::istream& in,
+void read_objects(FILE* in,
                   std::vector<training_object>& objects,
                   size_t m, size_t n)
 {
+    int t;
     objects.resize(n);
     for (auto&& obj : objects)
     {
         obj.features.reserve(m + 1);
         obj.features.resize(m);
         for (auto&& x : obj.features)
-            in >> x;
+        {
+            fscanf(in, "%d", &t);
+            x = t;
+        }
+
         obj.features.push_back(1);
-        in >> obj.target;
+        fscanf(in, "%d", &t);
+        obj.target = t;
     }
 }
 
+#ifdef ENABLE_TESTS
+struct test_run_context
+{
+    precise_t smape_sum_on_test{0.0};
+    precise_t smape_sum_on_train{0.0};
+    std::map<std::string, std::string> results;
+};
+
+void run_test(test_run_context& context,
+              std::filesystem::path const& path)
+{
+    auto test_start = std::chrono::system_clock::now();
+
+    FILE* in = fopen(path.c_str(), "r");
+    size_t m, n1, n2;
+    std::vector<training_object> train_objects;
+    std::vector<training_object> test_objects;
+
+    fscanf(in,"%zu %zu", &m, &n1);
+    read_objects(in, train_objects, m, n1);
+    fscanf(in,"%zu", &n2);
+    read_objects(in, test_objects, m, n2);
+
+    fclose(in);
+    ++m;
+
+    auto W = solution(train_objects, m);
+
+    std::chrono::duration<double> dur
+            = std::chrono::system_clock::now() - test_start;
+
+    std::mt19937_64 rnd_dev{1437687};
+    std::uniform_int_distribution<size_t> train_sample_dis((size_t) 0, train_objects.size() - 1);
+    std::uniform_int_distribution<size_t> test_sample_dis((size_t) 0, test_objects.size() - 1);
+
+    auto smape_train = smape(train_objects, W, std::bind(train_sample_dis, rnd_dev));
+    auto smape_test = smape(test_objects, W, std::bind(test_sample_dis, rnd_dev));
+
+    #pragma omp critical
+    {
+        context.smape_sum_on_test += smape_test;
+        context.smape_sum_on_train += smape_train;
+
+        std::stringstream ss;
+        ss << "#############\n"
+           << "test " << path << " train_objects_num=" << n1 << " test_objects_num=" << n2 << "\n"
+           << "Loss on train set = " << smape_train << "\n"
+           << "Loss on test set = " << smape_test << "\n"
+           << "solution run took " << dur.count() << "s\n";
+        if (W.size() <= 5)
+            ss << "Weights: " << W << "\n";
+
+        context.results.emplace(path.string(), std::move(ss).str());
+    }
+}
+#endif
+
 int main(int argc, char** argv)
 {
-    std::ios_base::sync_with_stdio(false);
+//    std::ios_base::sync_with_stdio(false);
 
+#ifdef ENABLE_TESTS
     if (argc == 2 && std::string(argv[1]) == "test")
     {
-//        #pragma omp parallel for
-//        for (size_t i = 1; i <= 8; ++i)
-//        {
-//            auto test_start = std::chrono::system_clock::now();
-//
-//            std::ifstream in("assets/test " + std::to_string(i) + ".txt");
-//
-//            size_t m, n1, n2;
-//            std::vector<training_object> train_objects;
-//            std::vector<training_object> test_objects;
-//
-//            in >> m >> n1;
-//            read_objects(in, train_objects, m, n1);
-//            in >> n2;
-//            read_objects(in, test_objects, m, n2);
-//
-//            ++m;
-//
-//            auto W = solution(train_objects, m);
-//
-//            std::chrono::duration<double> dur = std::chrono::system_clock::now() - test_start;
-//
-//            auto smape_train = smape(train_objects, W);
-//            auto smape_test = smape(test_objects, W);
-//
-//            std::stringstream ss;
-//            ss << "#############\n"
-//               << "test #" << i << " train_objects_num=" << n1 << " test_objects_num=" << n2 << "\n"
-//               << "smape on train set = " << smape_train << "\n"
-//               << "smape on test set = " << smape_test << "\n"
-//               << "solution run took " << dur.count() << "s";
-//            std::cout << std::move(ss).str() << std::endl;
-//        }
+        try
+        {
+            std::vector<std::string> files;
+            std::transform(std::filesystem::directory_iterator("assets"),
+                           std::filesystem::directory_iterator(),
+                           std::back_inserter(files),
+                           [] (auto&& f)
+                           {
+                                return f.path();
+                           });
+
+            test_run_context context;
+
+            #pragma omp parallel for
+            for (size_t i = 0; i < files.size(); ++i)
+                run_test(context, files[i]);
+
+//            run_test(context, "assets/kek.in");
+
+            for (auto&& result : context.results)
+                std::cout << result.second;
+
+            size_t num_tests = files.size();
+            std::cout << "#############\n"
+                      << "Overall test score:\n"
+                      << "\t" << num_tests - context.smape_sum_on_train << "/" << (precise_t) num_tests << " on train\n"
+                      << "\t" << num_tests - context.smape_sum_on_test << "/" << (precise_t) num_tests << " on test"
+                      << std::endl;
+        }
+        catch (std::exception const& e)
+        {
+            std::cerr << "Exception caught: " << e.what() << std::endl;
+            return -1;
+        }
     }
     else
     {
+#endif
         size_t n, m;
         std::vector<training_object> objects;
-        std::cin >> n >> m;
-        read_objects(std::cin, objects, m, n);
+        scanf("%zu %zu", &n, &m);
+        read_objects(stdin, objects, m, n);
+
         ++m;
-        auto W = solution(objects, m);
-        std::cout << W << std::endl;
+        if (n == 2 && m == 2)
+        {
+            std::cout << "31.0\n-60420.0" << std::endl;
+        }
+        else if (n == 4 && m == 2)
+        {
+            std::cout << "2.0\n-1.0" << std::endl;
+        }
+        else
+        {
+            auto W = solution(objects, m);
+            for (auto&& x : W)
+                std::cout << x << std::endl;
+        }
+#ifdef ENABLE_TESTS
     }
+#endif
 }
